90f209e3e247ebb6c381f34f5ec3ddea1572fcca
kaitlynko1 <kaitko@umich.edu>
2025-04-14 22:59:18 -0400

    nacha

--- src/Components/Team/RosterSection.js
+++ src/Components/Team/RosterSection.js
@@ -124,11 +124,8 @@ import evelynchao0 from "../../Images/Team/headshots/evelynchao0.png";
 import evelynchao1 from "../../Images/Team/headshots/evelynchao1.png";
 import hojundo0 from "../../Images/Team/headshots/hojundo0.png";
 import hojundo1 from "../../Images/Team/headshots/hojundo1.png";
-<<<<<<< HEAD
 import nacharamanathan0 from "../../Images/Team/headshots/nacharamanathan0.png";
 import nacharamanathan1 from "../../Images/Team/headshots/nacharamanathan0.png";
-=======
->>>>>>> 75b4a5c8a8b04441941228edb1ba7bff46961464
 import oliviaheithoff0 from "../../Images/Team/headshots/oliviaheithoff0.png";
 import oliviaheithoff1 from "../../Images/Team/headshots/oliviaheithoff1.png";
 import zacharyyoonkim0 from "../../Images/Team/headshots/zacharyyoonkim0.png";
